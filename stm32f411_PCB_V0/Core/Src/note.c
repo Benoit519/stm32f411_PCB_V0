59379231bf85d8f652552367fec12180ee81fc24
Benoit VIGNAUD <benoit.vignaud2@gmail.com>
2026-07-22 14:01:42 +0200

    CORRECTED NOTE;C

--- stm32f411_PCB_V0/Core/Src/note.c
+++ stm32f411_PCB_V0/Core/Src/note.c
@@ -4,27 +4,6 @@
 
 typedef struct
 {
-    const char *name;
-    float frequency;
-
-} NoteEntry;
-
-
-/*
-   Accordage standard A4 = 440 Hz
-
-   Do1 = C1
-   ...
-   Do10 = C10
-*/
-
-
-#include "note.h"
-#include <string.h>
-
-
-typedef struct
-{
     const char* name;
     float frequency;
 
@@ -144,26 +123,6 @@ float Note_GetFrequency(const char* name)
     for (int i = 0;i < NOTE_COUNT;i++)
     {
         if (strcmp(note_table[i].name, name) == 0)
-        {
-            return note_table[i].frequency;
-        }
-    }
-
-
-    // note inconnue
-    return 0.0f;
-}
-
-
-#define NOTE_COUNT (sizeof(note_table)/sizeof(NoteEntry))
-
-
-float Note_GetFrequency(const char *name)
-{
-
-    for(int i=0;i<NOTE_COUNT;i++)
-    {
-        if(strcmp(note_table[i].name,name)==0)
         {
             return note_table[i].frequency;
         }
